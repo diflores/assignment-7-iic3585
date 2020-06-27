f79c6624dfeef261604e68bbad1ffe6f1b2a8930
jatrijori <jtjorquera@uc.cl>
2020-06-27 17:40:45 -0400

    env var still not working

--- index.js
+++
@@ -1,5 +0,0 @@
-// import axios from "axios";
-// const dotenv = require('dotenv');
-// dotenv.config();
-console.log('ashdfjdsk')
-console.log(process.env.MOVIE_API_BASE_URL)
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import axios from "axios";
+// import dotenv from 'dotenv'
+// console.log(dotenv.config())
+// dotenv.config()
+// console.log(process.env.MOVIE_API_BASE_URL)
+console.log('ashdfjdsk')
